18f73367e919c0c07c6e7e4ab9fa4fed3b919d65
user <user@userdeMacBook-Pro.local>
2017-09-21 22:29:33 +0800

    修改错误

--- demo/nine-rectangle-grid/index.js
+++ demo/nine-rectangle-grid/index.js
@@ -1,51 +1,3 @@
-// var Select = {
-//     createNew: function() {　　　　　　
-//         var item = {};
-//         item.name = "大毛";
-//         // this.interval = {}
-//         item.starRandom = function() {
-//             let items = document.getElementsByClassName('item')
-//             let count = items.length
-//             this.interval = setInterval(function(){
-//             	for(let i=0;i<items.length;i++){
-//             		items[i].style.setProperty('background', '#fda529');
-//             	}
-// 	            var arr = [];
-// 	            while (arr.length < 3) {
-// 	                var bFlag = true;
-// 	                var number = Math.floor(Math.random() * count);
-// 	                for (var i = 0; i < arr.length; i++) {
-// 	                    if (number == arr[i]) {
-// 	                        bFlag = false;
-// 	                    }
-// 	                }
-// 	                if (bFlag) {
-// 	                    arr.push(number);
-// 	                }
-// 	            }
-//             	for(let i=0;i<3;i++){
-// 	            	let color = item.getRandomColor()
-// 	            	for(let j=0;j<arr.length;j++){
-// 	            		let eleIndex = arr[j]
-// 	            		items[eleIndex].style.setProperty('background', color);
-// 	            	}
-// 	            }
-//             },1000)
-//         };
-//         item.getRandomColor = function() {
-//         	var hex = Math.floor(Math.random() * 16777216).toString(16); //生成ffffff以内16进制数
-//             while (hex.length < 6) { //while循环判断hex位数，少于6位前面加0凑够6位
-//                 hex = '0' + hex;
-//             }
-//             return '#' + hex; //返回‘#'开头16进制颜色
-//         }
-//         item.stop = function(){
-//         	console.log(item)
-//         	clearInterval(this.interval)
-//         }
-//         return item
-//     }
-// }
 var btnAction = {
     interval : '',
     start:function(){
@@ -64,6 +16,7 @@ var btnAction = {
             items[i].style.setProperty('background', '#fda529');
         }
         clearInterval(this.interval)
+        this.interval = ''
     },
     getRandomColor : function() {
      var hex = Math.floor(Math.random() * 16777216).toString(16); //生成ffffff以内16进制数
@@ -93,10 +46,8 @@ var btnAction = {
         }
         for(let i=0;i<3;i++){
             let color = this.getRandomColor()
-            for(let j=0;j<arr.length;j++){
-                let eleIndex = arr[j]
-                items[eleIndex].style.setProperty('background', color);
-            }
+            let eleIndex = arr[i]
+            items[eleIndex].style.setProperty('background', color);
         }
     }
 }
